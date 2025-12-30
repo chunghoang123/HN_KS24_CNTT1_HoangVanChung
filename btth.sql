1f1c906863111b331ae5f0efa589d4b897f27e86
chunghoang123 <chungss7890@gmail.com>
2025-12-30 07:54:47 +0700

    Add files via upload

--- btth.sql
+++ btth.sql
@@ -2,6 +2,8 @@
 use btth;
 
 
+drop table Reader;
+drop table Borrow;
 create table Reader(
 	reader_id int  auto_increment primary key,
     reader_name varchar(100) not null,
@@ -32,20 +34,21 @@ alter table Reader add email varchar(100) unique;
 alter table Book modify author varchar(150) not null;
 
 
-insert into Reader 
-value 
+insert into Reader (reader_id, reader_name, phone, email, register_date)
+values
 (1,'Nguyễn Văn An','0901234567','an.nguyen@gmail.com','2024-09-01'),
 (2,'Trần Thị Bình','0912345678','binh.tran@gmail.com','2024-09-05'),
 (3,'Lê Minh Châu','09012345613','chau.le@gmail.com','2023-09-01');
 
-insert into Book
+
+insert into Book(book_id,book_title,author,publish_year)
 value
 (101,'Lập trình C căn bản','Nguyễn Văn A','2018'),
 (102,'Cơ sở dữ liệu','Nguyễn Văn B','2015'),
 (103,'Lập trình Java ','Nguyễn Văn C','2011'),	
 (104,'Hệ quản trị MySQL','Nguyễn Văn D','2019');
 
-insert into Book
+insert into Borrow(reader_id,book_id,borrow_date,return_date)
 value
 (1,'101','2024-09-15',''),
 (1,'102','2024-09-15','2024-09-25'),
